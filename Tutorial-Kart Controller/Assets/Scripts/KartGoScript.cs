c66663b6fe9d345b1bb5fd229ab71d6fb9c30ec4
Kyla Kubik <89461900+kylakubik2@users.noreply.github.com>
2023-02-22 16:39:10 -0600

    Added lap/coin counts

--- Tutorial-Kart Controller/Assets/Scripts/KartGoScript.cs
+++ Tutorial-Kart Controller/Assets/Scripts/KartGoScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
+using TMPro;
 
 public class KartGoScript : MonoBehaviour
 {
@@ -24,10 +25,23 @@ public class KartGoScript : MonoBehaviour
     private float CurrentSpeed = 0;
     public float MaxSpeed;
     private float RealSpeed;
+    public float originalMaxSpeed;
 
     private float steerDirection;
     Vector3 steerDirVect;
     float steerAmount;
+
+    private int coinCount;
+    private int numLaps;
+    public TextMeshProUGUI coinCountText;
+    public TextMeshProUGUI lapCountText;
+    private float startEffectTime;
+
+    public AudioSource source;
+    public AudioClip coinClip;
+    public AudioClip boostClip;
+    public AudioClip bombClip;
+    public AudioClip cheer;
 
     void Start()
     {
@@ -41,6 +55,13 @@ public class KartGoScript : MonoBehaviour
 
         kartTurnRight.AddOnStateDownListener(RightTurnDown, rightHand);
         kartTurnRight.AddOnStateUpListener(RightTurnDown, rightHand);
+
+        coinCount = 0;
+        numLaps = 0;
+        originalMaxSpeed = MaxSpeed;
+
+        SetCountText();
+        SetLapText();
     }
 
 
@@ -92,6 +113,44 @@ public class KartGoScript : MonoBehaviour
     }
 
 
+    void SetCountText()
+    {
+        coinCountText.text = "Coins: " + coinCount.ToString();
+    }
+    void SetLapText()
+    {
+        lapCountText.text = "Lap: " + numLaps.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            coinCount = coinCount + 1;
+
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Mushroom"))
+        {
+            other.gameObject.SetActive(false);
+            startEffectTime = Time.time;
+            MaxSpeed = MaxSpeed * 1.25f;
+        }
+        else if (other.gameObject.CompareTag("Bomb"))
+        {
+            other.gameObject.SetActive(false);
+            startEffectTime = Time.time;
+            MaxSpeed = MaxSpeed * 0.5f;
+        }
+        else if (other.gameObject.CompareTag("FinishLine"))
+        {
+            numLaps = numLaps + 1;
+
+            SetLapText();
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 vel = transform.forward * CurrentSpeed;
@@ -102,5 +161,10 @@ public class KartGoScript : MonoBehaviour
         steerAmount = RealSpeed > 30 ? RealSpeed / 4 * steerDirection : steerAmount = RealSpeed / 1.5f * steerDirection;
         steerDirVect = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + steerAmount, transform.eulerAngles.z);
         transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, steerDirVect, 3 * Time.deltaTime);
+
+        if (Time.time >= (startEffectTime + 5.0f))
+        {
+            MaxSpeed = originalMaxSpeed;
+        }
     }
 }
